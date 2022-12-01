fc0265007c0368afb615d8d493b657f3c618c516
moonyujin <mj1h10000@naver.com>
2022-12-01 13:50:10 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 이 부분을 채울 겁니다!
+    path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
 ]
